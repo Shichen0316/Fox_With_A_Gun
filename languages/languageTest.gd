8b15cf584657fd8dc543b3a97db8afacae6c3464
Eniko Agotai <agotai.enci@gmail.com>
2024-04-09 19:33:09 +0200

    v.0 of language manager

---
+++ languages/languageTest.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func _ready():
+	# directly use the LanguageManager autoload singleton to set the button's text
+	$Button.text = LanguageManager.get_text("thisisatest")
+
+func _on_button_pressed():
+	if LanguageManager.current_language == "EN":
+		LanguageManager.set_language("HU")
+	else:
+		LanguageManager.set_language("EN")
+	$Button.text = LanguageManager.get_text("thisisatest")
