520648162efa65076d2367145ea3d0ba9dcd28c3
Olle Nordlander <85176361+onjuno@users.noreply.github.com>
2024-05-10 17:03:27 +0200

    Added Controls

--- views/options_menu.gd
+++ views/options_menu.gd
@@ -1,4 +1,9 @@
 extends Control
+@onready var show_controls = $showControls
+@onready var options_menu = $optionsMenu
+
+
+
 
 func _on_language_pressed():
 	pass # Replace with function body.
@@ -9,12 +14,17 @@ func _on_resolution_pressed():
 
 
 func _on_controls_pressed():
-	pass # Replace with function body.
-
-
+	show_controls.visible = true
+	options_menu.visible = false
+	
 func _on_volume_pressed():
 	pass # Replace with function body.
 
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://views/menu.tscn")
+
+
+func _on_return_pressed():
+	show_controls.visible = false
+	options_menu.visible = true
