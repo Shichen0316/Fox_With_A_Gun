e54f782a6f9afef5d6b3e499a13b7fe3559efa70
Olle Nordlander <85176361+onjuno@users.noreply.github.com>
2024-05-13 12:43:27 +0200

    Options changes & added sound

--- views/menu.gd
+++ views/menu.gd
@@ -1,20 +1,25 @@
 extends Control
+@onready var button_click = $buttonClick
 
 
 
 
 
 func _on_play_pressed():
+	button_click.play()
 	get_tree().change_scene_to_file("res://Level/game_level.tscn")
 
 
 func _on_tutorial_pressed():
+	button_click.play()
 	pass
 
 
 func _on_options_pressed():
+	button_click.play()
 	get_tree().change_scene_to_file("res://views/options_menu.tscn")
 
 
 func _on_quit_pressed():
+	button_click.play()
 	get_tree().quit()
--- views/options_menu.gd
+++ views/options_menu.gd
@@ -1,30 +1,81 @@
 extends Control
 @onready var show_controls = $showControls
 @onready var options_menu = $optionsMenu
+@onready var back_from_resolution = $showResolution/VBoxContainer/backFromResolution
+@onready var show_resolution = $showResolution
+@onready var back_from_language = $showLanguage/VBoxContainer/backFromLanguage
+@onready var show_language = $showLanguage
+@onready var button_click = $buttonClick
 
 
 
 
 func _on_language_pressed():
-	pass # Replace with function body.
-
+	button_click.play()
+	show_language.visible = true
+	options_menu.visible = false
 
 func _on_resolution_pressed():
-	pass # Replace with function body.
-
+	button_click.play()
+	show_resolution.visible = true
+	options_menu.visible = false
 
 func _on_controls_pressed():
+	button_click.play()
 	show_controls.visible = true
 	options_menu.visible = false
-	
-func _on_volume_pressed():
-	pass # Replace with function body.
+
 
 
 func _on_back_pressed():
+	button_click.play()
 	get_tree().change_scene_to_file("res://views/menu.tscn")
 
 
 func _on_return_pressed():
+	button_click.play()
 	show_controls.visible = false
 	options_menu.visible = true
+
+func _on_back_from_resolution_pressed():
+	button_click.play()
+	show_resolution.visible = false
+	options_menu.visible = true
+	
+
+
+func _on_back_from_language_pressed():
+	button_click.play()
+	show_language.visible = false
+	options_menu.visible = true
+
+
+
+
+func _on_english_pressed():
+	
+	#add script to replace UI and language
+	button_click.play()
+	show_language.visible = false
+	options_menu.visible = true
+
+
+
+func _on_hungarian_pressed():
+	
+	
+	#add script to replace UI and language
+	button_click.play()
+	show_language.visible = false
+	options_menu.visible = true
+	
+
+
+func _on_swedish_pressed():
+	
+	
+	#add script to replace UI and language
+	button_click.play()
+	show_language.visible = false
+	options_menu.visible = true
+	
